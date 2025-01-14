7e1f3ca7588cbcdbcab4bf577519ef06fe2650b1
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-15 08:18:13 +0900

    fix : modify outputs name

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "project" {
   description = "IAM 멤버가 적용된 GCP 프로젝트 ID"
-  value       = google_project_iam_member.iam_member.project
+  value       = google_project_iam_member.project_iam_member.project
 }
 
 output "member" {
   description = "IAM 멤버 ID"
-  value       = google_project_iam_member.iam_member.member
+  value       = google_project_iam_member.project_iam_member.member
 }
 
 output "role" {
   description = "IAM 멤버에 할당된 역할"
-  value       = google_project_iam_member.iam_member.role
+  value       = google_project_iam_member.project_iam_member.role
 }
 
 output "condition" {
